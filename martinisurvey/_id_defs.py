35187612824403e10939f4f78719d63b9d90c5ec
Kyle Oman <koman@astro.rug.nl>
2019-03-15 11:26:49 +0000

    Flag extra line for edit.

--- martinisurvey/_id_defs.py
+++ martinisurvey/_id_defs.py
@@ -1,4 +1,4 @@
 from collections import namedtuple
 
-snap_id = namedtuple('snap_id', ['res', 'phys', 'vol', 'snap'])
-obj_id = namedtuple('obj_id', ['fof', 'sub'])
+snap_id = namedtuple('snap_id', ['res', 'phys', 'vol', 'snap'])  # EDIT
+obj_id = namedtuple('obj_id', ['fof', 'sub'])  # EDIT
